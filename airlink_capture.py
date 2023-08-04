309e52bff6263d789fa74ba0c0463278c1ed1139
Boris Resnick <boris@flyver.city>
2023-08-04 15:49:27 +0000

    Enable|disable threads params

--- airlink_capture.py
+++ airlink_capture.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from threading import Thread, Event, Lock
 import logging as lg
 import json
+import datetime
 
 import signal_capture
 import coord_capture
@@ -17,7 +18,9 @@ class Logger:
         self.log = None
 
     def __enter__(self):
-        self.log = open('data/log.txt', 'a')
+        now = datetime.datetime.utcnow().timetuple()
+        date_str = time.strftime('%Y-%m-%d_%H-%M-%S', now)
+        self.log = open(f'data/log_{date_str}.txt', 'w+')
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -46,19 +49,24 @@ def main():
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('--src', type=int, default=coord_capture.default_src())
     parser.add_argument('--remote', type=str, default='127.0.0.1:14560')
+    parser.add_argument('--nomavlink', action='store_true', default=False)
+    parser.add_argument('--nomodem', action='store_true', default=False)
     args = parser.parse_args()
     lg.basicConfig(level=lg.DEBUG if args.verbose else lg.INFO)
     event = Event()
 
     with Logger(args) as logger:
-        coords = coord_capture.CoordCapture(args, event, logger)
-        coord_thread = Thread(target=coords.run)
-        coord_thread.daemon = True
-        coord_thread.start()
-        signal = signal_capture.SignalCapture(args, event, logger)
-        signal_thread = Thread(target=signal.run)
-        signal_thread.daemon = True
-        signal_thread.start()
+        if not args.nomavlink:
+            coords = coord_capture.CoordCapture(args, event, logger)
+            coord_thread = Thread(target=coords.run)
+            coord_thread.daemon = True
+            coord_thread.start()
+
+        if not args.nomodem:
+            signal = signal_capture.SignalCapture(args, event, logger)
+            signal_thread = Thread(target=signal.run)
+            signal_thread.daemon = True
+            signal_thread.start()
 
         while True:
             try:
@@ -74,8 +82,11 @@ def main():
                 event.set()
                 break
 
-        coord_thread.join()
-        signal_thread.join()
+        if not args.nomavlink:
+            coord_thread.join()
+
+        if not args.nomodem:
+            signal_thread.join()
 
 
 if __name__ == '__main__':
